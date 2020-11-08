06ebd578c45649ca77e6e6fe8f7f8421fb81550a
Ninja99 <36740602+Ninja73737@users.noreply.github.com>
2020-11-08 15:48:41 -0500

    Removed python ta checks

--- .scripts/macos_highlight_and_accent.py
+++ .scripts/macos_highlight_and_accent.py
@@ -156,18 +156,5 @@ def set_closest_color(hex_color: str) -> None:
 
 
 if __name__ == '__main__':
-    import python_ta
-
-    python_ta.check_all(config={
-        'max-line-length': 100,
-        'max-args': 6,
-        'max-locals': 25,
-        'disable': ['R1705'],
-    })
-
     import doctest
     doctest.testmod(verbose=True)
-
-    import python_ta.contracts
-    python_ta.contracts.DEBUG_CONTRACTS = False
-    python_ta.contracts.check_all_contracts()
