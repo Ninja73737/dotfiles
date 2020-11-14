aca1f681fb0b6a6f220cdccce30b4c4a6b3f1503
mtoohey31 <mtoohey31@users.noreply.github.com>
2020-11-14 09:41:20 -0500

    Added interactivity test to everything except path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,102 +10,106 @@ end
 
 set -g fish_user_paths "$HOME/.scripts/" $fish_user_paths
 
-# Aliases
+if status --is-interactive
 
-# Time (mean ± σ):      11.1 ms ±   1.2 ms    [User: 7.3 ms, System: 3.4 ms]
-# Range (min … max):    10.1 ms …  18.3 ms    230 runs
+    # Aliases
 
-if string match -rq ".*MANJARO-ARM.*" (uname -r)
-    alias selfhosting "git --git-dir=$HOME/selfhosting --work-tree=$HOME"
+    # Time (mean ± σ):      11.1 ms ±   1.2 ms    [User: 7.3 ms, System: 3.4 ms]
+    # Range (min … max):    10.1 ms …  18.3 ms    230 runs
 
-    alias dcu "docker-compose --env-file docker-compose.env up -d"
-    alias dcd "docker-compose down --remove-orphans"
-end
+    if string match -rq ".*MANJARO-ARM.*" (uname -r)
+        alias selfhosting "git --git-dir=$HOME/selfhosting --work-tree=$HOME"
 
-alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+        alias dcu "docker-compose --env-file docker-compose.env up -d"
+        alias dcd "docker-compose down --remove-orphans"
+    end
 
-if test -d $HOME/.termux
-    alias ls "exa -a --group-directories-first"
-    alias lsd "exa -al group-directories-first"
-    alias lst "exa -aT -L 5 group-directories-first"
-    alias lsta "exa -aT group-directories-first"
-else if not string match -rq ".*ish.*" (uname -r)
-    alias ls "exa -a --icons --group-directories-first"
-    alias lsd "exa -al --icons --group-directories-first"
-    alias lst "exa -aT -L 5 --icons --group-directories-first"
-    alias lsta "exa -aT --icons --group-directories-first"
-end
+    alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
-# Fish Settings
+    if test -d $HOME/.termux
+        alias ls "exa -a --group-directories-first"
+        alias lsd "exa -al group-directories-first"
+        alias lst "exa -aT -L 5 group-directories-first"
+        alias lsta "exa -aT group-directories-first"
+    else if not string match -rq ".*ish.*" (uname -r)
+        alias ls "exa -a --icons --group-directories-first"
+        alias lsd "exa -al --icons --group-directories-first"
+        alias lst "exa -aT -L 5 --icons --group-directories-first"
+        alias lsta "exa -aT --icons --group-directories-first"
+    end
 
-# Time (mean ± σ):      17.1 ms ±   1.4 ms    [User: 13.8 ms, System: 2.9 ms]
-# Range (min … max):    16.1 ms …  29.0 ms    153 runs
+    # Fish Settings
 
-fish_vi_key_bindings
+    # Time (mean ± σ):      17.1 ms ±   1.4 ms    [User: 13.8 ms, System: 2.9 ms]
+    # Range (min … max):    16.1 ms …  29.0 ms    153 runs
 
-set fish_cursor_default line
-set fish_cursor_insert line
-set fish_cursor_replace_one line
+    fish_vi_key_bindings
 
-export VISUAL=nvim
-export EDITOR=nvim
+    set fish_cursor_default line
+    set fish_cursor_insert line
+    set fish_cursor_replace_one line
 
-# Update wal Colours
+    export VISUAL=nvim
+    export EDITOR=nvim
 
-# Time (mean ± σ):     185.4 ms ±   8.4 ms    [User: 114.4 ms, System: 58.0 ms]
-# Range (min … max):   178.4 ms … 207.3 ms    16 runs
+    # Update wal Colours
 
-if test (uname) = "Darwin"
-    set current_wallpaper (sqlite3 -readonly ~/Library/Application\ Support/Dock/desktoppicture.db 'SELECT * FROM data ORDER BY rowID DESC LIMIT 1;')
-    set previous_wallpaper (cat ~/.config/wal/previous_wallpaper)
+    # Time (mean ± σ):     185.4 ms ±   8.4 ms    [User: 114.4 ms, System: 58.0 ms]
+    # Range (min … max):   178.4 ms … 207.3 ms    16 runs
 
-    set dark_mode (defaults read -g AppleInterfaceStyle 2>/dev/null)
-    # set dark_mode (osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
-    set previous_mode (cat ~/.config/wal/previous_mode)
+    if test (uname) = "Darwin"
+        set current_wallpaper (sqlite3 -readonly ~/Library/Application\ Support/Dock/desktoppicture.db 'SELECT * FROM data ORDER BY rowID DESC LIMIT 1;')
+        set previous_wallpaper (cat ~/.config/wal/previous_wallpaper)
 
-    if test "$dark_mode" != "$previous_mode" -o $current_wallpaper != $previous_wallpaper
-        wal -c
-        if test -z "$dark_mode"
-        # if test "$dark_mode" = "false"
-            wal -i $current_wallpaper -nql
+        set dark_mode (defaults read -g AppleInterfaceStyle 2>/dev/null)
+        # set dark_mode (osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
+        set previous_mode (cat ~/.config/wal/previous_mode)
+
+        if test "$dark_mode" != "$previous_mode" -o $current_wallpaper != $previous_wallpaper
+            wal -c
+            if test -z "$dark_mode"
+            # if test "$dark_mode" = "false"
+                wal -i $current_wallpaper -nql
+            else
+                wal -i $current_wallpaper -nq
+            end
+            update_spicetify
+            set_macos_accent
+            # neofetch > ~/.config/neofetch
         else
-            wal -i $current_wallpaper -nq
+            wal -Rnq
         end
-        update_spicetify
-        set_macos_accent
-        # neofetch > ~/.config/neofetch
-    else
+
+        echo $current_wallpaper > ~/.config/wal/previous_wallpaper
+
+        echo $dark_mode > ~/.config/wal/previous_mode
+    else if string match -rq ".*MANJARO-ARM.*" (uname -r)
         wal -Rnq
     end
 
-    echo $current_wallpaper > ~/.config/wal/previous_wallpaper
+    # Clear screen if running inside ranger
 
-    echo $dark_mode > ~/.config/wal/previous_mode
-else if string match -rq ".*MANJARO-ARM.*" (uname -r)
-    wal -Rnq
-end
+    # Time (mean ± σ):       5.5 ms ±   0.6 ms    [User: 3.5 ms, System: 1.6 ms]
+    # Range (min … max):     4.8 ms …  10.3 ms    386 runs
 
-# Clear screen if running inside ranger
-
-# Time (mean ± σ):       5.5 ms ±   0.6 ms    [User: 3.5 ms, System: 1.6 ms]
-# Range (min … max):     4.8 ms …  10.3 ms    386 runs
-
-if test -n "$RANGER_LEVEL"
-    clear
-end
-
-# Pretty Stuff
-
-if test -n "$SSH_CLIENT"
-    clear
-end
-
-if not string match -rq ".*ish.*" (uname -r)
-    if not test -d $HOME/.termux
-        source ~/.cache/wal/colors.fish
+    if test -n "$RANGER_LEVEL"
+        clear
     end
 
-    neofetch
+    # Pretty Stuff
 
-    starship init fish | source
+    if test -n "$SSH_CLIENT"
+        clear
+    end
+
+    if not string match -rq ".*ish.*" (uname -r)
+        if not test -d $HOME/.termux
+            source ~/.cache/wal/colors.fish
+        end
+
+        neofetch
+
+        starship init fish | source
+    end
+
 end
