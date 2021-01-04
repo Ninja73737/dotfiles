73cda6ca605b7e40ee356cb05286658cb6dd3def
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-01-03 20:11:24 -0500

    Added remove orphans to dcu shortcut

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,20 @@
 
 if test (uname) = "Darwin"
     set -g fish_user_paths "/usr/local/opt/arm-gcc-bin@8/bin" $fish_user_paths
-    set -g fish_user_paths "/Users/mtoohey/Library/Python/3.8/bin" $fish_user_paths
+    set -g fish_user_paths "$HOME/Library/Python/3.8/bin" $fish_user_paths
+    set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 end
-
 set -g fish_user_paths "$HOME/.scripts/" $fish_user_paths
 
 if status --is-interactive
 
     # Aliases
 
-    alias dcu "docker-compose --env-file docker-compose.env up -d"
+    if test (uname) = "Darwin"
+        alias cdic "cd '/Users/mtoohey/Library/Mobile Documents/com~apple~CloudDocs'"
+    end
+
+    alias dcu "docker-compose --env-file docker-compose.env up -d --remove-orphans"
     alias dcd "docker-compose down --remove-orphans"
 
     if string match -rq ".*MANJARO-ARM.*" (uname -r)
@@ -54,25 +58,26 @@ end
     # Update wal Colours
 
     if test (uname) = "Darwin"
-        # wal --theme base16-nord -nq
+        # # wal --theme base16-nord -nq
 
-        # set current_wallpaper (sqlite3 -readonly ~/Library/Application\ Support/Dock/desktoppicture.db 'SELECT * FROM data ORDER BY rowID DESC LIMIT 1;')
-        set current_wallpaper (osascript -e 'tell app "finder" to get posix path of (get desktop picture as alias)')
+        # # set current_wallpaper (sqlite3 -readonly ~/Library/Application\ Support/Dock/desktoppicture.db 'SELECT * FROM data ORDER BY rowID DESC LIMIT 1;')
+        # set current_wallpaper (osascript -e 'tell app "finder" to get posix path of (get desktop picture as alias)')
 
-        set dark_mode (defaults read -g AppleInterfaceStyle 2>/dev/null)
-        # set dark_mode (osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
+        # set dark_mode (defaults read -g AppleInterfaceStyle 2>/dev/null)
+        # # set dark_mode (osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
 
-        if test -z "$dark_mode"
-        # if test "$dark_mode" = "false"
-            wal -i $current_wallpaper -nql
-        else
-            wal -i $current_wallpaper -nq
-        end
-        update_spicetify
-
+        # if test -z "$dark_mode"
+        # # if test "$dark_mode" = "false"
+        #     wal -i $current_wallpaper -nql
+        # else
+        #     wal -i $current_wallpaper -nq
+        # end
+        # update_spicetify
     else if string match -rq ".*MANJARO-ARM.*" (uname -r)
-        wal --theme base16-nord -q
+        # wal --theme base16-nord -q
     end
+
+    wal -Rnq
 
     # Clear screen if running inside ranger or over ssh
 
