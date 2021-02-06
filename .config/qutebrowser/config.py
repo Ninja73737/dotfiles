58d6fd9d1b7ed24c0ee4c25894eed78fec81e84d
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-02-06 07:58:42 -0500

    Added qutebrowser configuration

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,3 @@
+config.bind('<Alt-o>', 'set-cmd-text -s :open --window')
+config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+c.editor.command = ['alacritty', '-e', 'fish', '-C', "nvim {file}"]
