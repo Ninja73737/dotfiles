e27b8dcdf5d32cf763c05b785bcaeacf6506d9ae
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-02-19 22:54:58 -0500

    Disabled qutebrowser autoconfig loading so I don't rely on it

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,4 @@
+config.load_autoconfig(False)
 config.bind('<Alt-o>', 'set-cmd-text -s :open --window')
 config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
 c.editor.command = ['alacritty', '-e', 'fish', '-C', "nvim {file}"]
