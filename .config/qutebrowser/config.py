23c6b4d4845baed7dd25fdea313067b636b29270
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-03-27 20:01:11 -0400

    Switched to qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,19 @@
+# Misc
 config.load_autoconfig(False)
-config.bind('<Alt-o>', 'set-cmd-text -s :open --window')
-config.bind('<Ctrl-Shift-y>', 'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.set('statusbar.show', 'in-mode')
+config.set('auto_save.session', True)
 c.editor.command = ['alacritty', '-e', 'fish', '-C', "nvim {file}"]
+
+# Keybinds
+config.bind('<Ctrl-o>', 'set-cmd-text -s :open --window')
+config.bind('<Ctrl-Shift-y>',
+            'hint links spawn --detach mpv --force-window yes {hint-url}')
+config.bind('D', 'close')
+config.bind('B', 'spawn --userscript qute-bitwarden')
+
+# Theming
+config.set('colors.webpage.preferred_color_scheme', 'dark')
+config.source('qutewal.py')
+
+c.fonts.default_family = 'FiraCode Nerd Font'
+c.fonts.default_size = '12pt'
