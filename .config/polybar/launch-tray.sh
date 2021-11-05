6203a9f7ce44dde1a9b38439fd94ae40883680aa
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-11-05 19:08:55 -0400

    remove X11 stuff

--- .config/polybar/launch-tray.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 0.1; done
-
-source $HOME/.cache/wal/colors.sh
-bspc config ignore_ewmh_struts true
-
-polybar -c $HOME/.config/polybar/config.ini tray &
